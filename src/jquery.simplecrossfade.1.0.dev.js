c0b31c7d6b75901a3a99473639fd09436b147531
Tomoyuki Tsujimoto <tomo.tsujimoto@gmail.com>
2016-02-09 18:03:26 +0900

    Change indent

--- src/jquery.simplecrossfade.1.0.dev.js
+++ src/jquery.simplecrossfade.1.0.dev.js
@@ -1,72 +1,72 @@
 /**
- * jQuery simplecrossfade v1.0.1 - 2012-11-28
+ * jQuery simplecrossfade v1.0.1
  * https://github.com/tomothumb/jquery.simplecrossfade
- * (c) 2012 Tomoyuki Tsujmioto
+ * (c) 2016 Tomoyuki Tsujmioto
  * license: www.opensource.org/licenses/mit-license.php
  */
 ;(function($) {
 
-  $.fn.simplecrossfade = function(options) {
+    $.fn.simplecrossfade = function(options) {
 
-    var elements = this;
-    // OPTION
-    var opts = $.extend( {}, $.fn.simplecrossfade.defaults, options );
+        var elements = this;
+        // OPTION
+        var opts = $.extend( {}, $.fn.simplecrossfade.defaults, options );
 
-    elements.each(function() {
-      thumb = $(this);
+        elements.each(function() {
+            thumb = $(this);
 
-      // Change main new image
-      var changeImg = function (){
-        newimg = $("<img/>")
-                  .attr("src", $(this).attr("src").replace(opts.suffix_small,opts.suffix_large) )
-                  .fadeIn(opts.speed_fadein).addClass("fadein");
-        $(opts.target).parent().find(".fadeactive").fadeOut(opts.speed_fadeout)
-                      .end()
-                      .prepend(newimg)
-                      ;
-      } // --> End of functioo[changeImg]
+            // Change main new image
+            var changeImg = function (){
+                newimg = $("<img/>")
+                    .attr("src", $(this).attr("src").replace(opts.suffix_small,opts.suffix_large) )
+                    .fadeIn(opts.speed_fadein).addClass("fadein");
+                $(opts.target).parent().find(".fadeactive").fadeOut(opts.speed_fadeout)
+                    .end()
+                    .prepend(newimg)
+                ;
+            } // --> End of functioo[changeImg]
 
-      // Remove main old image
-      var removeImg = function (){
-        $(opts.target).parent()
-                      .find(".fadeactive")
-                        .remove().end()
-                      .find(".fadein")
-                        .removeClass(".fadein").addClass("fadeactive")
-                       ;
-      } // --> End of functioo[removeImg]
+            // Remove main old image
+            var removeImg = function (){
+                $(opts.target).parent()
+                    .find(".fadeactive")
+                    .remove().end()
+                    .find(".fadein")
+                    .removeClass(".fadein").addClass("fadeactive")
+                ;
+            } // --> End of functioo[removeImg]
 
-      // event driven
-      thumb.on("mouseover", changeImg);
-      thumb.on("mouseout", removeImg);
+            // event driven
+            thumb.on("mouseover", changeImg);
+            thumb.on("mouseout", removeImg);
 
-    }); // --> end of each
+        }); // --> end of each
 
-    $(window).on("load", function(){
-      $(opts.target).addClass("fadeactive")
-                    .css({
-                      'margin': 0
-                    })
-                    .parent()
-                    .css({
-                      'height': $(opts.target).height()
-                    })
-                    ;
-    });
+        $(window).on("load", function(){
+            $(opts.target).addClass("fadeactive")
+                .css({
+                    'margin': 0
+                })
+                .parent()
+                .css({
+                    'height': $(opts.target).height()
+                })
+            ;
+        });
 
 
-    // method chain
-    return this;
+        // method chain
+        return this;
 
-  }; // --> End of plugin[simplecrossfade]
+    }; // --> End of plugin[simplecrossfade]
 
-  // Default setting of seplugin[simplecrossfade]
-  $.fn.simplecrossfade.defaults = {
-    suffix_small  : "small",          // suffix of thumnail images
-    suffix_large  : "large",          // suffix of thumnail images
-    target        : "div#mainimg img", // jquery selector of img which you want to crossfade
-    speed_fadeout : 500,              // fadeout speed which old image hides
-    speed_fadein  : 300               // fadein speed when new image appears
-  };
+    // Default setting of seplugin[simplecrossfade]
+    $.fn.simplecrossfade.defaults = {
+        suffix_small  : "small",          // suffix of thumnail images
+        suffix_large  : "large",          // suffix of thumnail images
+        target        : "div#mainimg img", // jquery selector of img which you want to crossfade
+        speed_fadeout : 500,              // fadeout speed which old image hides
+        speed_fadein  : 300               // fadein speed when new image appears
+    };
 
-}(jQuery))
+}(jQuery));
